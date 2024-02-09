97948cf138b1ce6e087393ca307dc7d4a3dfc21d
Rik Dijkstra <rik.dijkstra@hotmail.com>
2024-02-09 16:44:47 +0100

    initial functionality

--- scripts/module.js
+++ scripts/module.js
@@ -1,6 +1,25 @@
 (() => { })();
 
 Hooks.once('init', async function() {
+    // Assuming you're within an async function
+    const templatePath = "modules/combat-encounter-balancer-5e/templates/dialogs/select-folder-dialog.html";
+    const templateHtml = await fetch(templatePath).then(response => response.text());
+
+    // Render the template HTML in a dialog
+    let dialog = new Dialog({
+    title: "Select Folder",
+    content: templateHtml,
+    buttons: {
+        select: {
+        label: "Select",
+        callback: html => {
+            // Handle selection
+        }
+        }
+    },
+    default: "select"
+    });
+    dialog.render(true);
 
 });
 
